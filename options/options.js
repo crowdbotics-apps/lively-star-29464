9822edf5285237802954090ffdfe7ea89736c7cd
Crowdbotics <team@crowdbotics.com>
2021-08-04 17:58:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lively_star_29464Identifier",
+  url: "https://lively_star_29464Identifier.botics.co",
+  api: "https://lively_star_29464Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
